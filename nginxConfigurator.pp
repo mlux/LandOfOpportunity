8be24373bd7a9e7383a4c082820078bfa7d28168
mlux <michaellux@hotmail.com>
2023-02-07 13:39:12 -0500

    Update nginxConfigurator.pp

--- nginxConfigurator.pp
+++ nginxConfigurator.pp
@@ -3,32 +3,20 @@
 $epp_source_path = '/etc/puppetlabs/code/environments/production/files'
 $default_sblock_path = '/etc/puppetlabs/code/environments/production/templates'
 $vhost = 'www.anothertestsite.com'
-$doc_root = '/usr/share/nginx'
-#$doc_root = '/var/www/html'
-#$create_dirs = ['/var/','/var/www/',"${doc_root}"]
+$doc_root = '/var/www/html'
+$create_dirs = ['/var/','/var/www/',"${doc_root}"]
 
 
 node puppet.target.com {
 
 	# This creates the directory path for $doc_root if needed
-#	file {$create_dirs:
-#		ensure => "directory",
-#		owner => 'root',
-#		group => 'root',
-#		mode => '0777',
-#	}
-
-	# This function installs the nginx package
-	package {'nginx':
-		ensure => 'installed',
+	file {$create_dirs:
+		ensure => "directory",
+		owner => 'root',
+		group => 'root',
+		mode => '0777',
 	}
-	
-	# Creates a status.txt file to include the content "Nginx Installed"
-	file {'/tmp/status.txt':
-		content => 'Nginx Installed',
-		mode => '0644',
-	}
-	
+		
 	# Defines the service "nginx" and verifies it is running and enabled
 	service {"nginx":
 		ensure => 'running',
@@ -43,7 +31,7 @@ node puppet.target.com {
 		mode => '0777',
 	}
 	
-	# This creates the file "index.html" and populates it with the contents found 	in our specified epp file
+	# This creates the file "index.html" and populates it with the contents found in our specified epp file
 	file {"${doc_root}/${vhost}/index.html":
 		ensure => "file",
 		owner => 'root',
