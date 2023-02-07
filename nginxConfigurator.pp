b0aac3ee77ad42c9dbfa98417f7f34dca84d23d8
mlux <michaellux@hotmail.com>
2023-02-07 13:35:53 -0500

    Update nginxConfigurator.pp

--- nginxConfigurator.pp
+++ nginxConfigurator.pp
@@ -1,10 +1,1 @@
-node puppet.target.com {
 
-  package {'nginx':
-    ensure => present,
-  }
-  
-  nginx::vhost { 'www.anothertestsite.com':
-    docroot => 'var/www/html/www.anothertestsite.com',
-  }
-}
