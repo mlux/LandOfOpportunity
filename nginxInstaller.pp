f943f3e9fdc0e994be4b3770e1dac06ead384084
mlux <michaellux@hotmail.com>
2023-02-07 04:37:31 -0500

    Update nginxInstaller.pp

--- nginxInstaller.pp
+++ nginxInstaller.pp
@@ -1,11 +1,54 @@
-node www.actestsite.com {
-
-	package { 'nginx':
-		ensure=> 'installed',
+node puppet.target.com {
+	package {'nginx':
+		ensure => 'installed',
 	}
-	
+
 	file {'/tmp/status.txt':
 		content => 'Nginx Installed',
 		mode => '0644',
 	}
+
+#	service {"nginx":
+#		ensure => 'running',
+#		enable => true,
+#	}
+
+	file {'/usr/share/nginx/actestsite.com':
+		ensure => "directory",
+		owner => 'root',
+		group => 'root',
+		mode => '0777',
+	}
+
+	file {'/usr/share/nginx/actestsite.com/index.html':
+		ensure => "file",
+		owner => 'root',
+		group => 'root',
+		mode => '0777',
+		content => epp('/etc/puppetlabs/code/environments/production/files/index.html.epp'),
+	}
+
+	file {'/etc/nginx/conf.d/actestsite.com.conf':
+		ensure => "file",
+		owner => 'root',
+		group => 'root',
+		mode => '0755',
+		content => epp('/etc/puppetlabs/code/environments/production/files/actestsite.com.conf.epp'),
+	}
+
+	file {"/etc/hosts":
+		content => epp("/etc/puppetlabs/code/environments/production/files/hosts.epp"),
+	}
+
+	file {"/etc/nginx/nginx.conf":
+		content => epp("/etc/puppetlabs/code/environments/production/templates/nginx.conf.epp"),
+	}
+
+	service { 'nginx':
+		restart => 'systemctl restart nginx',
+	}
+
 }
+
+
+
